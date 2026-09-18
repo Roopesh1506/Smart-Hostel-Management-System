1859a037da32750620fad52f0e06e15916effdc6
Roopesh Singh <roopeshrameshsingh15@gmail.com>
2026-09-18 22:14:26 +0530

    Initial Smart Hostel Management System

---
+++ src/main/java/com/smarthostel/Main.java
@@ -0,0 +1,15 @@
+package com.smarthostel;
+
+import com.smarthostel.ui.LoginFrame;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            try { UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName()); }
+            catch (Exception ignored) {}
+            new LoginFrame().setVisible(true);
+        });
+    }
+}
